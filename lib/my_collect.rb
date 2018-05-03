a40830fd31426ceb3b5ab9906b1e1229dbc91643
Jathmel Gordon <jathmel@gmail.com>
2018-05-03 01:41:44 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   result_arr = [] 
   i = 0 
   while i < array.length 
-    result_arr << yield array[i]
+    result_arr << (yield array[i])
     i += 1 
   end 
   result_arr
